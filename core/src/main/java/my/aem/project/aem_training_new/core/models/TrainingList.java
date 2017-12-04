28e19185a328f0f838a735b5a1dfb034c535866a
André Sundberg <andre.l.sundberg@accenture.com>
2017-12-04 13:18:12 +0100

    Training List

--- core/src/main/java/my/aem/project/aem_training_new/core/models/TrainingList.java
+++ core/src/main/java/my/aem/project/aem_training_new/core/models/TrainingList.java
@@ -2,10 +2,17 @@ package my.aem.project.aem_training_new.core.models;
 
 import my.aem.project.aem_training_new.core.models.Training;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.time.temporal.TemporalAdjusters;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
@@ -23,13 +30,21 @@ public class TrainingList {
 	@ScriptVariable
 	private Page currentPage;
 	
+	private static final String TITLE = "jcr:trainingTitle";
+	private static final String TYPE = "jcr:trainingType";
+	private static final String DATE = "jcr:trainingDate";
+	private static final String LOCATION = "jcr:location";
+	private static final String DESCRIPTION = "jcr:trainingDescription";
+	
+	private String path = "jcr:content/root/trainingdetails";
 	private String value;
 	private List<Training> trainings;
 	private List<String> months;
 	
-
+	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	private LocalDateTime currentDate;
 	
-	public Collection<Training> getTrainings() {
+	public Collection<Training> getTrainings() throws ParseException {
 		if(trainings == null) {
 			trainings = new ArrayList<Training>();
 			createTraining();
@@ -37,33 +52,42 @@ public class TrainingList {
 		return trainings;
 	}
 	
-	public void createTraining() {
+	public void createTraining() throws ParseException {
 	Iterator<Page> childPages = currentPage.listChildren();
 	while(childPages.hasNext()) {
 		Page child = childPages.next();
-		Training training = new Training(getValue("jcr:title", child), getValue("jcr:type", child), getValue("jcr:date", child), getValue("jcr:location", child), getValue("jcr:description", child));
+		Date date = sdf.parse(getValue(DATE, child));
+		Training training = new Training(getValue(TITLE, child), getValue(TYPE, child), date, getValue(LOCATION, child), getValue(DESCRIPTION, child));
 		trainings.add(training);
 		}
 	}
 	
 	public String getValue(String name, Page page) {
 		Resource resource = page.adaptTo(Resource.class);
-		ValueMap properties = resource.getChild("jcr:content/root/trainingdetails").getValueMap();
+		ValueMap properties = resource.getChild(path).getValueMap();
 		value = properties.get(name, String.class);
 		return value;
 	}
 	
 	public List<String> getMonths() {
+		LocalDate localDate;
+		String date;
+		String month;
+		
 		months = new ArrayList<String>();
+		currentDate = LocalDateTime.now();
 		
-		String month1 = "April";
-		String month2 = "May";
-		String month3 = "June";
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
 		
-		months.add(month1);
-		months.add(month2);
-		months.add(month3);
+		date = dtf.format(currentDate);
+		localDate = LocalDate.parse(date, dtf.ofPattern("yyyy/MM/dd"));
 		
+		
+		for(int i = 0; i < 3; i++) {
+			month = localDate.format(dtf.ofPattern("MMMM")).toString();
+			months.add(month);
+			localDate = localDate.with(TemporalAdjusters.firstDayOfNextMonth());
+		}
 		return months;
 	}
 }
